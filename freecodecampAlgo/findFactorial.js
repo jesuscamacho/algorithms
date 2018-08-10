68820de5f12f6277d3cabd8ca899a414b8ec3b27
Jesus Camacho <33095879+jesuscb18@users.noreply.github.com>
2018-08-09 23:09:04 -0400

    test

---
+++ freecodecampAlgo/findFactorial.js
@@ -0,0 +1,11 @@
+function factorialize(num) {
+    if(num==0){
+        return 1;
+    }
+    else if(num == 1){
+        return num *1
+    }
+    return num * factorialize(num-1);
+  }
+  
+  console.log(factorialize(5))
