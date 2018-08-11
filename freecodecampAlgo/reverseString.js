0ec90619ce1dfd596c4a3151dbbdb22515c72154
Jesus Camacho <33095879+jesuscb18@users.noreply.github.com>
2018-08-11 18:05:28 -0400

    moved to basic folder

--- freecodecampAlgo/reverseString.js
+++
@@ -1,9 +0,0 @@
-function reverseString(str) {
-    var newstr = ""
-    for(var i=str.length-1;i>=0;i--){
-      newstr += str[i]
-    }
-    return newstr;
-  }
-
-  console.log(reverseString('hello'))
